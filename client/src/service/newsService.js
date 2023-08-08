cb810b5f98ef12a7af21802d2dc39fed0282f09d
Michael-Ber <mikeber000@gmail.com>
2023-08-08 16:42:52 +0800

    change urls to render

--- client/src/service/newsService.js
+++ client/src/service/newsService.js
@@ -1,10 +1,8 @@
 export const newsService = () => {
-    // const apiKey = '7c5bf92a51e04e629562b39598462ba4';
-
-    // const apiUrlHeadlines = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&pageSize=${pageSize}&apiKey=${apiKey}`;
-    // const apiUrlEverything = `https://newsapi.org/v2/everything?q=${category}&language=${language}&apiKey=${apiKey}`;
-    const apiUrlHeadlines = `http://localhost:3005/`;
-    const apiUrlEverything = `http://localhost:3005/search_result`;
+    const apiUrlHeadlines = `https://news-24aj.onrender.com/`;
+    const apiUrlEverything = `https://news-24aj.onrender.com/search_result`;
+    // const apiUrlHeadlines = `http://localhost:3005/`;
+    // const apiUrlEverything = `http://localhost:3005/search_result`;
 
     return {
         apiUrlHeadlines,
