a510b4f2f26d1f323b5a84d3821e5f6d14cec3fe
Бершадский Михаил Сергеевич <Michail.Bershadsky@ilimtimber.corp>
2023-09-06 11:21:18 +0800

    load more now working properly, carousel working properly, need redone search component

--- server/controllers/news.js
+++ server/controllers/news.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { v4 as uuidv4 } from 'uuid';
 
 const apiKey = '7c5bf92a51e04e629562b39598462ba4';
 
@@ -8,10 +9,10 @@ export const getHeadlineNews = async(req, res) => {
         const { country, category, pageSize, page } = req.body;
         const URL = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&page=${page}&pageSize=${pageSize}&apiKey=${apiKey}`;
         const articles = await axios.get(URL);
-        const articlesToClient = articles.data.articles.length !== 0 ? articles.data.articles : [];
+        const articlesToClient = articles.data.articles.length !== 0 && articles.data.articles.filter(article => article.description !== "[Removed]").map(article => ({...article, id: uuidv4()}));
         return res.json({message: "Send OK", articles: articlesToClient});
     } catch (error) {
-        res.json({message: "Error while getting news"})
+        res.json({message: "Error while getting news",error})
     }
 } 
 
