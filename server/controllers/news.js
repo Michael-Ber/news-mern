72d741868c4eb7022c4d5c9cae50a580d0d188ad
Бершадский Михаил Сергеевич <Michail.Bershadsky@ilimtimber.corp>
2023-10-12 09:37:27 +0800

    fix1

--- server/controllers/news.js
+++ server/controllers/news.js
@@ -3,27 +3,27 @@ import { v4 as uuidv4 } from 'uuid';
 
 const apiKey = '7c5bf92a51e04e629562b39598462ba4';
 
-export const getHeadlineNews = async(req, res) => {
-
+export const getHeadlineNews = async (req, res) => {
+    console.log(req.params);
     try {
         const { country, category, pageSize, page } = req.body;
         const URL = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&page=${page}&pageSize=${pageSize}&apiKey=${apiKey}`;
         const articles = await axios.get(URL);
-        const articlesToClient = articles.data.articles.length !== 0 && articles.data.articles.filter(article => article.description !== "[Removed]").map(article => ({...article, id: uuidv4()}));
-        return res.json({message: "Send OK", articles: articlesToClient ? articlesToClient: []});
+        const articlesToClient = articles.data.articles.length !== 0 && articles.data.articles.filter(article => article.description !== "[Removed]").map(article => ({ ...article, id: uuidv4() }));
+        return res.json({ message: "Send OK", articles: articlesToClient ? articlesToClient : [] });
     } catch (error) {
-        res.json({message: "Error while getting news",error})
+        res.json({ message: "Error while getting news", error })
     }
-} 
+}
 
-export const getSearchedNews = async(req, res) => {
+export const getSearchedNews = async (req, res) => {
     try {
         const { category, language } = req.body;
         const URL = `https://newsapi.org/v2/everything?q=${category}&language=${language}&apiKey=${apiKey}`;
         const articles = await axios.get(URL);
-        const articlesToClient = articles.data.articles.length !== 0 && articles.data.articles.filter(article => article.description !== "[Removed]").map(article => ({...article, id: uuidv4()}));
-        return res.json({message: "Send OK", articles: articlesToClient});
+        const articlesToClient = articles.data.articles.length !== 0 && articles.data.articles.filter(article => article.description !== "[Removed]").map(article => ({ ...article, id: uuidv4() }));
+        return res.json({ message: "Send OK", articles: articlesToClient });
     } catch (error) {
-        res.json({message: "Error while getting news"})
+        res.json({ message: "Error while getting news" })
     }
 } 
--- server/routes/news.js
+++ server/routes/news.js
@@ -3,7 +3,7 @@ import { getHeadlineNews, getSearchedNews } from "../controllers/news.js";
 
 const router = new Router();
 
-router.post('/category', getHeadlineNews);
+router.post('/category/:category', getHeadlineNews);
 router.post('/search_result', getSearchedNews);
 
 export default router;
